475a73af0934a60232f0329e2711524a65b36544
rolando <rgs-00@outlook.com>
2024-05-26 16:55:28 -0600

    Creating burbuja file and bubbleSort script

---
+++ Java/AlgoritmosOrdenamiento/Burbuja.java
@@ -0,0 +1,25 @@
+package AlgoritmosOrdenamiento;
+
+import java.util.ArrayList;
+
+
+public class Burbuja {
+    public ArrayList<Integer> ordenar(ArrayList<Integer> numeros){
+        if(numeros.isEmpty()){
+            return numeros;
+        }else{
+            boolean ordenado = true;
+            do {
+                for (int i = 0; i < numeros.size() - 2; i++) {
+                    if(numeros.get(i) > numeros.get(i + 1)){
+                        int aux = numeros.get(i + 1);
+                        numeros.set(i + 1, numeros.get(i));
+                        numeros.set(i, aux);
+                        ordenado = false;
+                    }
+                }
+            }while(!ordenado);
+            return numeros;
+        }
+    }
+}
